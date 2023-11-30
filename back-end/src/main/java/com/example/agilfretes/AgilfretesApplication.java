a54cb55110d77911fa28e64ef3d8309b32bb1db3
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-11-30 15:24:06 -0300

    creating backend with Java Spring

---
+++ back-end/src/main/java/com/example/agilfretes/AgilfretesApplication.java
@@ -0,0 +1,13 @@
+package com.example.agilfretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilfretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilfretesApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/example/agilfretes/AgilfretesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agilfretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilfretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
