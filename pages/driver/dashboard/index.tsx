938ea73d6a4fc586cf40df37bf82f1ecec5d5ee3
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-12-06 17:47:37 -0300

    creating driver dashboard

--- pages/driver/dashboard/index.tsx
+++ pages/driver/dashboard/index.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import CrudFreightsDrivers from '../../../components/CrudFreightsDrivers'
+
 export default function DashboardDriver() {
   return (
     <main
       className={`min-h-screen p-24 ${inter.className}`}
     >
-      <h1>Bem vindo Entregador</h1>
-      <h2>Dashboard</h2>
+      <CrudFreightsDrivers />
     </main>
   )
 }
