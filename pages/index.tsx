dc361caec705fd07c5e4ca014f4159483c818ebe
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-12-06 17:43:40 -0300

    styling home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,27 +4,14 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+
 export default function Home() {
   return (
-    <main
-      className={`min-h-screen p-24 ${inter.className}`}
-    >
-      <div className="flex">
-        <Link
-          className="mr-5 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-          href="/admin"
-        >
-          Administradores
-        </Link>
-
-        <Link
-          className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-          href="/driver"
-        >
-          Entregadores
-        </Link>
-      </div>
-      
-    </main>
+    <>
+    <Header />
+    <Banner /> 
+    </>
   )
 }
