52a7d99dedb04196caba40932333c2d70643229d
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-12-07 08:55:45 -0300

    refactoring routes

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,21 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function LoginAdmin() {
+import CrudProducts from '../../components/CrudProducts'
+import CrudVehicleTypes from '../../components/CrudVehicleTypes'
+import CrudFreights from '../../components/CrudFreights'
+import Header from '../../components/Header'
+
+export default function Admin() {
+  
   return (
-    <main
-      className={`min-h-screen p-24 ${inter.className}`}
-    >
-      <h1>Bem vindo Administrador</h1>
-      <Link
-        className="mr-5 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-        href="/"
+    <>
+      <Header />
+      <main
+        className={`min-h-screen p-24 pt-0 ${inter.className}`}
       >
-        Home
-      </Link>
-      
-    </main>
-  )
+        <h1 className="text-3xl mt-5 mb-5 font-bold">Bem vindo, administrador</h1>
+        <CrudProducts />
+        <CrudVehicleTypes />
+        <CrudFreights />
+      </main>
+    </>
+  );
 }
+
+
+
--- pages/driver/index.tsx
+++ pages/driver/index.tsx
@@ -3,19 +3,20 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function LoginDriver() {
+import CrudFreightsDrivers from '../../components/CrudFreightsDrivers'
+import Header from '../../components/Header'
+
+export default function Driver() {
   return (
-    <main
-      className={`min-h-screen p-24 ${inter.className}`}
-    >
-      <h1>Bem vindo Entregador</h1>
-      <Link
-        className="mr-5 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-        href="/"
+    <>
+      <Header />
+      <main
+        className={`min-h-screen p-24 pt-0 ${inter.className}`}
       >
-        Home
-      </Link>
-      
-    </main>
+        <h1 className="text-3xl mt-5 mb-5 font-bold">Bem vindo, entregador</h1>
+        <CrudFreightsDrivers />
+      </main>
+    </>
   )
 }
+
